2b5f0c52dacc4bf89a2edec313673d9a13288c34
Rohanwagh5 <rohanwagh52005@gmail.com>
2025-07-19 14:55:47 +0530

    preview changed

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,12 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  origin: process.env.CLIENT_URL || "http://localhost:5173" ,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
+
 app.use(cors(corsOptions));
 
 app.use(express.json());
